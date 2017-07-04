5e56fcab6211bca6006b22b879650985811f04b0
geraldzakwan <geraldi_dzakwan@yahoo.co.id>
2017-07-04 14:45:00 +0900

    modify google search script to take two inputs and calculate co-occurence metrics

--- get_request_ex_1.py
+++ get_request_ex_1.py
@@ -1,25 +1,28 @@
+# Reference:
+# https://github.com/google/google-api-python-client/blob/master/samples/customsearch/main.py
+
+import sys
 from apiclient.discovery import build
 
 api_key = 'AIzaSyDkSCA8agvst49gtKEjrxRnI57lCqV9vBM'
 cx_key = '013692161269702497108:vgnkfyixhmo'
 
-def get_total_results():
+def get_total_results(word):
     service = build("customsearch", "v1", developerKey=api_key)
 
-    res = service.cse().list(q='Tokyo AND Kyoto', cx=cx_key,).execute()
+    res = service.cse().list(q=word, cx=cx_key,).execute()
 
-    total_results = res['queries']['request'][0]['totalResults']
+    total_results = float(res['queries']['request'][0]['totalResults'])
 
-    print(total_results)
+    return total_results
 
-    res = service.cse().list(q='Tokyo OR Kyoto', cx=cx_key,).execute()
+def co_occurence(word1, word2):
+    and_total_results = get_total_results(word1 + " AND " + word2)
+    or_total_results = get_total_results(word1 + " OR " + word2)
 
-    total_results = res['queries']['request'][0]['totalResults']
-
-    print(total_results)
-
-    # return total_results
+    return and_total_results/or_total_results
 
 if __name__ == '__main__':
-    # print(get_total_results())
-    get_total_results()
+    if(len(sys.argv) < 3):
+        sys.exit('Supply first word and second word')
+    print(co_occurence(sys.argv[1], sys.argv[2]))
